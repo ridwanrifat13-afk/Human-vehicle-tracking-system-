592b4f885f7bb2dde5caff904add6a17f7e22a38
Ridwan Rifat <ridwanrifat13@gmail.com>
2026-04-12 03:43:42 +0600

    Delete Human & vehicle tracker for drone. AG copy/vision_tracker directory

--- Human & vehicle tracker for drone. AG copy/vision_tracker/control/controller_interface.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class ControlInterface(ABC):
-    @abstractmethod
-    def send_target_position(self, x, y):
-        raise NotImplementedError
--- Human & vehicle tracker for drone. AG copy/vision_tracker/control/null_controller.py
+++
@@ -1,7 +0,0 @@
-from .controller_interface import ControlInterface
-
-
-class NullController(ControlInterface):
-    def send_target_position(self, x, y):
-        # This controller is a placeholder for future drone commands.
-        pass
